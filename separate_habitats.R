921a2db2bc10836727886052c90d455008c9c7b7
Graeme Duncan <GMDuncan@users.noreply.github.com>
2018-04-30 04:35:50 +0100

    pulled libraries from functions
    libraries in main code block and geometry changed to new SF type

--- separate_habitats.R
+++ separate_habitats.R
@@ -10,28 +10,29 @@
 ###############################################################
 ### IF YOU WISH TO JUST RUN THE SCRIPT, CHANGE THESE VALUES ###
 ###############################################################
-
+library(sf)
+library(dplyr)
+library(tidyr)
+library(tools)
 #Change to the folder where the shapefile is located
-file_location <- "C:/this_folder/this_subfolder"
-#Change to the filename
-file_name <- "this_file.shp"
+file_location <- "D:/My/Folder/Of/Shapefiles"
+#Change to the filename (minus the shp extension)
+file_name <- "GB001518"
 
 #Change to the character used to separate habitats in the original (if required)
 #For example, for mosaic "Sand+Mud", the separator would be "+"
-separator <- "+"
+separator <- "/"
 #Change to the field name containing the habitat values that you would like to split (if required)
 habitat_field <- "ORIG_HAB"
 
 #Change to the output folder where you want to save your shapefile
-out_folder <- "D:/my_folder"
+out_location <- "D:/"
 
 ###############################################################
 ###                     THEN RUN ALL BELOW                  ###
 ###############################################################
 
 separate_habitats <- function(input.dataset, fieldname = "ORIG_HAB",separator = "/"){
-  library(sf)
-  library(tidyr)  
   
   #Set Polygon values
   input.dataset$POLYGON <- seq.int(nrow(input.dataset))
@@ -49,7 +50,7 @@ separate_habitats <- function(input.dataset, fieldname = "ORIG_HAB",separator = 
   
   
   #Clean up and join polygons back in
-  final.df <- left_join(x=split.dataset[,c("GUI","POLYGON","ORIG_HAB","ORIG_CLASS","HAB_TYPE","VERSION","DET_MTHD","DET_NAME","DET_DATE","TRAN_COM","T_RELATE","VAL_COMM")],y=input.dataset[,c("POLYGON","Shape")], by="POLYGON")
+  final.df <- left_join(x=split.dataset[,c("GUI","POLYGON","ORIG_HAB","ORIG_CLASS","HAB_TYPE","VERSION","DET_MTHD","DET_NAME","DET_DATE","TRAN_COM","T_RELATE","VAL_COMM")],y=input.dataset[,c("POLYGON","geometry")], by="POLYGON")
   
   #Resort by POLYGON value
   final.df <- final.df[order(final.df$POLYGON),]
@@ -60,7 +61,7 @@ separate_habitats <- function(input.dataset, fieldname = "ORIG_HAB",separator = 
   return(final.df)
 }
 
-sf.dataset <- st_read(dsn=file_location, layer=file_name, stringsAsFactors = FALSE)
+sf.dataset <- st_read(dsn=file_location, layer=file_name, stringsAsFactors = FALSE, quiet = F)
 output.dataset <- separate_habitats(sf.dataset, fieldname=habitat_field, separator=separator)
 
 out_name <- paste("split_",file_name,sep="")
